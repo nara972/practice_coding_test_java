5c678e81d2a2757d1f80b81e1c817fbcb331c4fa
nara972 <68370958+nara972@users.noreply.github.com>
2024-02-27 21:12:41 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -2,17 +2,15 @@ class Solution {
     public int solution(int[] number) {
         int answer = 0;
         
-        for(int i = 0; i< number.length; i++){
-            for (int j = i+1; j< number.length; j++){
-                for(int k = j+1; k< number.length; k++){
+        for(int i=0;i<number.length;i++){
+            for(int j=0;j<i;j++){
+                for(int k=0;k<j;k++){
                     if(number[i]+number[j]+number[k]==0){
                         answer++;
                     }
                 }
             }
         }
-        
         return answer;
-        
     }
 }
