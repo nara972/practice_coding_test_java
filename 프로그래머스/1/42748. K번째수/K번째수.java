68631b9dddd8fb4b342e4f9fcf06ca98ca5458a4
nara972 <68370958+nara972@users.noreply.github.com>
2024-04-02 23:17:40 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,27 +4,21 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for(int i=0;i<commands.length;i++){
+        for(int i=0; i<commands.length;i++){
+            int startIndex = commands[i][0];
+            int endIndex = commands[i][1];
+            int selectIndex = commands[i][2];
             
-            int StartIndex=commands[i][0];
-            int EndIndex=commands[i][1];
-            int selectIndex=commands[i][2];
-            
-            int[] splitArray=new int[EndIndex-StartIndex+1];
-            
+            int[] splitArray = new int[endIndex-startIndex+1];
             int index=0;
-            
-            for(int j=StartIndex-1;j<EndIndex;j++){
+            for(int j =startIndex-1; j<endIndex;j++){
                 splitArray[index]=array[j];
                 index++;
             }
-            
             Arrays.sort(splitArray);
-            answer[i]=splitArray[selectIndex-1];
             
+            answer[i]=splitArray[selectIndex-1];
         }
-        
-    
         return answer;
     }
 }
