3f399c9cb73c3d7662a53bbc00f2b3183a486a00
nara972 <68370958+nara972@users.noreply.github.com>
2024-08-28 10:23:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 19.34 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,18 +1,18 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] strArr=s.split(" ");
+        String[] strArr = s.split(" ");
         
-        int[] intArr= new int[strArr.length];
-        
-        for(int i=0;i<strArr.length;i++){
-            intArr[i]=Integer.parseInt(strArr[i]);
+        int[] intArr = new int[strArr.length]; 
+            
+        for(int i=0; i<strArr.length;i++){
+            intArr[i] = Integer.parseInt(strArr[i]);
         }
         
-        int max=intArr[0];
-        int min=intArr[0];
+        int max = intArr[0];
+        int min = intArr[0];
         
-        for(int i=0; i<intArr.length; i++){
+        for(int i=0; i<intArr.length;i++){
             if(intArr[i]>max){
                 max=intArr[i];
             }
@@ -20,10 +20,6 @@ class Solution {
                 min=intArr[i];
             }
         }
-        
-        answer=min+" "+max;
-        
-        
-        return answer;
+        return min+" "+max;
     }
 }
