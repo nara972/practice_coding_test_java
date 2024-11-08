60365bcd0e61bf62b632a8e6044fbece63b10f01
nara972 <68370958+nara972@users.noreply.github.com>
2024-11-08 17:11:22 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 51.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int nowCount = Integer.bitCount(n);
+        
+        while(true){
+            n++;
+            int nextCount=Integer.bitCount(n);
+            
+            if(nowCount == nextCount){
+                break;
+            }
+        }
+        answer=n;
+        return answer;
+    }
+}
