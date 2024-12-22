1c65f704f1454450f891d36d202bebd26eca0516
nara972 <68370958+nara972@users.noreply.github.com>
2024-12-22 13:24:44 +0900

    [level 2] Title: 소수 찾기, Time: 24.94 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,44 +1,33 @@
-import java.util.*;
-
+import java.util.HashSet;
 class Solution {
     
-    HashSet<Integer> hs = new HashSet<>();
+    HashSet<Integer> set = new HashSet<>();
     
-    public int solution(String numbers) {
-        
-        rec("", numbers);
-        
-        int cnt = 0;
-        
-        
-        Iterator<Integer> ite = hs.iterator();
-        while (ite.hasNext()) {
-            int number = ite.next();
-            if(primeNumber(number)){
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-    
-    public void rec (String temp, String n){
-        
-        if(!temp.equals("")){
-            hs.add(Integer.parseInt(temp));
-        }
-        
-        for(int i = 0; i< n.length();i++){
-            rec(temp+n.charAt(i), n.substring(0,i)+ n.substring(i+1));
-        }
-    }
-    
-    static boolean primeNumber(int n){
-        if(n==0 || n ==1) return false;
-        for(int i =2; i< n; i++){
-            if(n%i==0){
-                return false;
-            }
+    public boolean isPrime(int n){
+        if(n<2) return false;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0) return false;
         }
         return true;
+    }
+    
+    public void recursive(String comb,String others){
+        
+        if(!comb.isEmpty()){
+            set.add(Integer.valueOf(comb));
+        }
+        
+        for(int i=0;i<others.length();i++){
+            recursive(comb+others.charAt(i),
+                     others.substring(0,i)+others.substring(i+1));
+        }
+    }
+    public int solution(String numbers) {
+        recursive("",numbers);
+        int cnt=0;
+        for(int n:set){
+            if(isPrime(n)) cnt++; 
+        }
+        return cnt;
     }
 }
