325ab12c96c71370998fe1b4deeb924c597cbe13
nara972 <68370958+nara972@users.noreply.github.com>
2025-01-02 23:54:13 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -7,10 +7,11 @@ class Solution {
         for(int i=0;i<clothes.length;i++){
             map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
         }
-        int answer=1;
+        int answer = 1;
         for(int count:map.values()){
             answer*=(count+1);
         }
+            
         return answer-1;
     }
 }
