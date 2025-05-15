1cea34433b63cd2e05cf4ba874bd139fd8dd725c
nara972 <68370958+nara972@users.noreply.github.com>
2025-05-15 21:03:11 +0900

    [level 2] Title: 올바른 괄호, Time: 10.61 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,23 @@
 class Solution {
     boolean solution(String s) {
+        boolean answer = true;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        //System.out.println("Hello Java");
         
-        int openCount=0;
-        int closeCount=0;
+        int openCount = 0;
+        int closeCount = 0;
         
-        for(int i=0; i<s.length();i++){
+        for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
                 openCount++;
             }else if(s.charAt(i)==')'){
                 closeCount++;
             }
-            
             if(openCount<closeCount){
                 return false;
             }
         }
-        
         if(openCount==closeCount){
             return true;
         }
